9ba7a028e8100ab4d4195503f740b7bbddc03e07
kshafhassan <49368455+kshafhassan@users.noreply.github.com>
2019-04-11 01:10:06 +0500

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,20 @@
+﻿function formValidation() {
+    var email = document.login.email;
+    var pass = document.login.password;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+    if (pass.value.length < 6) {
+        alert("Error: Password must contain at least six characters!");
+        pass.focus();
+        return false;
+    }
+
+
+}
---
+++ signup.js
@@ -0,0 +1,23 @@
+﻿function formValidation() {
+    var email = document.signup.email;
+    var pass = document.signup.password;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (re.test(pass)) {
+
+    }
+    else {
+        document.getElementById("messs").innerHTML = " // at least one number, one lowercase and one uppercase letter at least six characters "
+        return false;
+    }
+}
+
+}
